c50f961e1112e198fa84c3dfbf750eb2faa32a7d
tugi <blueqwinn@gmail.com>
2023-02-13 20:23:46 +0300

    sixth commit

--- lib/person.rb
+++ lib/person.rb
@@ -10,7 +10,7 @@ attr_reader :name
 end
 
 def name
-name = @name
+@name = name
 name.new ="Person's name"
 # => "Person's name"
  end
@@ -21,7 +21,7 @@ attr_reader :bank_account
 end
 
 def bank_account
-bank = @bank_account
+@bank = bank_account
 bank.new = "Value(25)"
 # => "Value(25)"
  end
